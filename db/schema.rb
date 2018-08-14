07d7150309875c10b20914cc73336e64dc58e5a2
EC2 Default User <ec2-user@ip-172-31-9-54.us-east-2.compute.internal>
2018-08-14 09:57:17 +0000

    new-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_075508) do
+ActiveRecord::Schema.define(version: 2018_08_14_094523) do
 
-  create_table "messages", force: :cascade do |t|
-    t.string "title"
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-  end
+# Could not dump table "messages" because of following StandardError
+#   Unknown type 'stirng' for column 'calorie'
 
 end
